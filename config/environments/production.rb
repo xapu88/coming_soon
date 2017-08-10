bac96b5b75034f5931d376c4d7e175f32b59548d
Hari Kalanj <hkalanj@gmail.com>
2017-08-10 21:21:33 +0200

    configure production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #ActionMailer configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "localhost",
+    :port                 => 25
+  }
+  config.action_mailer.default_options = {from: 'noreply@stylemyle.com'}
+  config.action_mailer.default_url_options = { host: 'stylemyle.com' }
 end
