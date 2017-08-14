bc778a101fa686b3e96e82c4b5de7a93ba98dc74
Hari Kalanj <hkalanj@gmail.com>
2017-08-15 01:55:47 +0200

    mailchimp integration

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 
+  respond_to :html, :js
+
   def index
     @subscriber = Subscriber.new
   end
@@ -7,8 +9,23 @@ class WelcomeController < ApplicationController
   def subscribe
     @subscriber = Subscriber.new(subscriber_params)
     if @subscriber.save
-      SubscriptionMailer.subscribe_email(@subscriber).deliver_later
-    else
+      # SubscriptionMailer.subscribe_email(@subscriber).deliver_later
+      @gb = Gibbon::Request.new
+      list = Rails.application.secrets.mailchimp[:list_id]
+      begin
+        @gb.lists(list).members.create(body: {email_address: @subscriber.email, status: "subscribed", double_optin: false})
+        respond_to do |format|
+          format.js { return }
+          format.html { redirect_to :back }
+        end
+      rescue Gibbon::MailChimpError => e
+        Rails.logger.debug("Subscribing failed: #{e.message}")
+        @error_message = get_error_message(e.title)
+        respond_to do |format|
+          format.js { render "failure.js.erb" }
+          format.html { redirect_to :back }
+        end
+      end
     end
   end
 
@@ -16,6 +33,16 @@ class WelcomeController < ApplicationController
 
   def subscriber_params
     params.require(:subscriber).permit(:email)
+  end
+
+  def get_error_message(title)
+    if title == "Member Exists"
+      return "Already subscribed. Thank you!"
+    elsif title == "Invalid Resource"
+      return "Email address not valid."
+    else
+      return "Subscribe failed. Please try again."
+    end
   end
 
 end
